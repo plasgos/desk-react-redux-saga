d4b22a2368ec9e5132d2f9f1e17c263de1950ae6
Dyan Kastutara <dyankastutara19@gmail.com>
2024-01-04 10:13:50 +0700

    add dummy

--- src/components/example/saga.js
+++ src/components/example/saga.js
@@ -1,7 +1,7 @@
 import { put, call, takeLatest } from 'redux-saga/effects';
 import types from './constant';
 import * as actions from './reducer';
-import dummy from './dummy.json';
+import addresses from '../dummy/addresses.json';
 
 function* watchGetAddress(values){
   const {payload} = values;
@@ -9,7 +9,7 @@ function* watchGetAddress(values){
   try{
     // const response = yield call(axios, payload)
     // const {data} = response;
-    yield put(actions.getAddressSuccess(dummy));
+    yield put(actions.getAddressSuccess(addresses));
   }catch(e){
     yield put(actions.setIsLoadingGetAddress(false))
   }finally{
